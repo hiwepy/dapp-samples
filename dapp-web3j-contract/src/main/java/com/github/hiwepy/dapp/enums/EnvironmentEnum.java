dbab55e5dd555be5b121798f415cd1c505952027
wandl <hiwepy@163.com>
2024-09-26 23:24:36 +0800

    feat：模块调整

---
+++ dapp-web3j-contract/src/main/java/com/github/hiwepy/dapp/enums/EnvironmentEnum.java
@@ -0,0 +1,15 @@
+package com.github.hiwepy.dapp.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum EnvironmentEnum {
+    PROD,
+    DEV,
+    TEST,
+    ;
+
+
+}
---
+++ dapp-web3j-contract/src/main/java/com/github/hiwepy/dapp/util/Contract.java
@@ -0,0 +1,23 @@
+package com.github.hiwepy.dapp.util;
+
+public abstract class Contract {
+
+    public static final String FUNC_DECIMALS = "decimals";
+    public static final String FUNC_BALANCE_OF = "balanceOf";
+    public static final String FUNC_APPROVE = "approve";
+    public static final String FUNC_CREATE_ORDER = "createOrder";
+    public static final String FUNC_DEPOSIT_FUND = "depositFund";
+    public static final String FUNC_SETTLE_ORDER = "settleOrder";
+    public static final String FUNC_BALANCES = "balances";
+    public static final String FUNC_CUMULATIVE_INCOME = "assetsManagement";
+    public static final String FUNC_BATCH_SETTLE_ORDERS = "batchSettleOrders";
+    public static final String FUNC_SLOT1 = "slot1";
+    public static final String FUNC_USER_NUMBER = "userNumber";
+    public static final String FUNC_ASSETS_MANAGEMENT = "assetsManagement";
+    public static final String FUNC_SLOT0 = "slot0";
+    public static final String FUNC_USER_INFO = "userInfo";
+    public static final String FUNC_CALCULATE_SHARES = "calculateShares";
+    public static final String FUNC_CALCULATE_AMOUNTS = "calculateAmounts";
+    public static final String FUNC_ORDER_INFO = "orderInfo";
+
+}
